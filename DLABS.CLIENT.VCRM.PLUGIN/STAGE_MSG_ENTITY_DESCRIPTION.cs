0dc802b8268c3306029c8323b7448c0282faec9f
Atili0 <atilio.rosas.e@gmail.com>
2018-05-14 15:19:27 +0200

    no message

---
+++ DLABS.CLIENT.VCRM.PLUGIN/STAGE_MSG_ENTITY_DESCRIPTION.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+    public class STAGE_MSG_ENTITY_DESCRIPTION
+{
+    }
+}
---
+++ JavaScript/My_Script1.js
@@ -0,0 +1,47 @@
+﻿var $fileinputname$ = (function () {
+    //Private properties & methods
+
+    //var x = 0;
+    //var _privateMethod = function() {
+    //    return x++;
+    //};
+
+    return {
+        Load: load,
+        Save: save,
+        function1: function () {
+            //TODO: Do stuff
+            //return x;
+        }
+    };
+
+    function load() {
+            attachEventsToFields();
+    }
+
+    function save() {
+    }
+
+    function attachEventsToFields() {
+
+        /*var field = S2G.Fields.GetField('ownerid');
+        if (field != null) {
+            field.addOnChange(onChangePropietario);
+        }
+        */
+    }
+
+})();
+
+function main_load() {
+    require.config({
+        paths: {
+            "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min",
+            "s2gfunction": "../WebResources/<folder>/js/s2gfunction"
+        }
+    });
+
+    require(['jquery', 's2gfunction'], function () {
+        relax_descuentos.Load();
+    });
+};
